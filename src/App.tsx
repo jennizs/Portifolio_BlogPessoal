fa7849f05915fbcea631bf2cdd157d2608d38174
jennizs <jennyfrn6104@gmail.com>
2024-05-17 13:52:27 -0300

    implementando o tailwind

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Navbar from './paginas/NavBar/NavBar'
+import Footer from './paginas/footer/footer'
+import Home from './paginas/home/home'
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/paginas/home/home.tsx
@@ -0,0 +1,59 @@
+function Home() {
+    return (
+        <>
+
+            <div className="
+            bg-indigo-900 
+            flex
+            justify-center">
+                <div className="
+                container grid
+                grid-cols-2
+                text-white">
+                    <div className="
+                    max-w-7xl 
+                    flex
+                    flex-col
+                    items-center 
+                    gap-4 
+                    justify-center 
+                    py-4">
+                        <h2 className="
+                        text-5xl 
+                        font-bold">Seja Bem Vinde!</h2>
+                        <p className="
+                        text-lg">Expresse aqui seus pensamentos</p>
+                        <div className="
+                        flex
+                        justify-around 
+                        gap-4">
+                            <div className="
+                            rounded
+                            text-white
+                            border-white 
+                            border-solid 
+                            border-2 
+                            py-2 
+                            px-4">
+
+                                Nova Postagem
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="
+                    flex 
+                    justify-center">
+                        <img
+                            src="https://i.imgur.com/VpwApCU.png"
+                            alt="" 
+                            className="w-2/3" />
+                    </div>
+                </div>
+            </div>
+        </>
+
+    );
+}
+
+export default Home;
