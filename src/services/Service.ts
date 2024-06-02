c6e52660d9cfdf7d6c3a74b54208b89e1f66cddb
jennizs <jennyfrn6104@gmail.com>
2024-06-02 20:09:09 -0300

    novo

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
+    
 });
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
     const resposta = await api.post(url, dados);
